0d502769301ecca44cf6131df146038250dfd763
John Anderton <janderton24@gmail.com>
2020-12-19 16:32:11 -0800

    modifies server/app.js file, require express, body-parser, adds port 3000, and creates a listen and get request

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+
+app.get('/highlight', (req, res) => {
+  console.log('GET');
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
